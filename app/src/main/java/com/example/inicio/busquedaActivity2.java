aaaf92ff4478dab9b88ed6f9d2de27d6cbe850ec
Emanuel Rendon H <18alexx1981@gmail.com>
2022-06-07 20:31:07 -0500

    mi primer commit

--- app/src/main/java/com/example/inicio/busquedaActivity2.java
+++ app/src/main/java/com/example/inicio/busquedaActivity2.java
@@ -36,10 +36,15 @@ public class busquedaActivity2 extends AppCompatActivity {
         else {
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             String nombre = busqueda2Binding.etnomb.getText().toString();
-            Cursor cursor = db.rawQuery("SELECT * FROM producto WHERE nombre ="+ nombre,null);
+            Cursor cursor = db.rawQuery("SELECT * FROM producto WHERE nombre = '"+ nombre + "'",null);
             cursor.moveToNext();
             busqueda2Binding.tvid.setText("id: "+ cursor.getInt(0));
             busqueda2Binding.etnom.setText("nombre: "+ cursor.getString(1));
+            busqueda2Binding.etpre.setText("precio: "+ cursor.getFloat(2));
+            busqueda2Binding.etdescrip.setText("descripcion: "+ cursor.getString(3));
+            busqueda2Binding.etref.setText("referencia: "+ cursor.getString(4));
+            busqueda2Binding.etcant.setText("cantidad: "+ cursor.getString(5));
+            busqueda2Binding.etcate.setText("categoria: "+ cursor.getString(6));
         }
 
     }
